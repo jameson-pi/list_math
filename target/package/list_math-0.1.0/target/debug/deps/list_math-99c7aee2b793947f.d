1da23cc522205523117da6f13ce963b9edbb6231
Jameson Piscitello <piscitello284@gmail.com>
2021-03-18 14:52:42 +0000

    fix spelling errors in Cargo.toml

---
+++ target/package/list_math-0.1.0/target/debug/deps/list_math-99c7aee2b793947f.d
@@ -0,0 +1,5 @@
+/home/runner/listmath/target/debug/deps/list_math-99c7aee2b793947f: src/lib.rs
+
+/home/runner/listmath/target/debug/deps/list_math-99c7aee2b793947f.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/package/list_math-0.1.0/target/debug/liblist_math.d
@@ -0,0 +1,1 @@
+/home/runner/listmath/target/package/list_math-0.1.0/target/debug/liblist_math.rlib: /home/runner/listmath/target/package/list_math-0.1.0/src/lib.rs
